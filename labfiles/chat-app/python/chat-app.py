b983312847932541eb15c0ba7307c844c467db86
user1-56549429 <User1-56549429@LODSPRODMSLEARNMCA.onmicrosoft.com>
2025-11-13 08:34:36 +0000

    ai102lab2

--- labfiles/chat-app/python/chat-app.py
+++ labfiles/chat-app/python/chat-app.py
@@ -2,6 +2,10 @@ import os
 from dotenv import load_dotenv
 
 # Add references
+# Add references
+from azure.identity import DefaultAzureCredential
+from azure.ai.projects import AIProjectClient
+from openai import AzureOpenAI
 
 
 def main(): 
@@ -17,13 +21,26 @@ def main():
         model_deployment =  os.getenv("MODEL_DEPLOYMENT")
 
         # Initialize the project client
-        
+        # Initialize the project client
+        project_client = AIProjectClient(            
+                credential=DefaultAzureCredential(
+                    exclude_environment_credential=True,
+                    exclude_managed_identity_credential=True
+                ),
+                endpoint=project_endpoint,
+            )
 
         # Get a chat client
+        # Get a chat client
+        openai_client = project_client.get_openai_client(api_version="2024-10-21")
+
 
 
         # Initialize prompt with system message
-         
+        # Initialize prompt with system message
+        prompt = [
+                {"role": "system", "content": "You are a helpful AI assistant that answers questions."}
+            ]
 
         # Loop until the user types 'quit'
         while True:
@@ -36,7 +53,14 @@ def main():
                 continue
             
             # Get a chat completion
-
+            # Get a chat completion
+            prompt.append({"role": "user", "content": input_text})
+            response = openai_client.chat.completions.create(
+                    model=model_deployment,
+                    messages=prompt)
+            completion = response.choices[0].message.content
+            print(completion)
+            prompt.append({"role": "assistant", "content": completion})
 
     except Exception as ex:
         print(ex)
