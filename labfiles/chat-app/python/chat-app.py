4e0b74095820ae09dc1a035ed4a9ad40d488b4e8
user1-56549429 <User1-56549429@LODSPRODMSLEARNMCA.onmicrosoft.com>
2025-11-13 08:38:57 +0000

    updated

--- labfiles/chat-app/python/chat-app.py
+++ labfiles/chat-app/python/chat-app.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-# Add references
+# Add references1
 # Add references
 from azure.identity import DefaultAzureCredential
 from azure.ai.projects import AIProjectClient
